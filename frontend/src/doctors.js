6a9c0a91f2f692fca20b9deedd0979450dbae798
KarolinaKolodziej22 <k.kolodziej01@gmail.com>
2023-01-22 19:54:48 +0100

    adding doctor example, applying more details

--- frontend/src/doctors.js
+++ frontend/src/doctors.js
@@ -3,82 +3,166 @@ const doctors = [
       '_id': '1',
       'name': 'Ortopeda',
       'description': 'Złamania, urazy, dolegliwości układu ruchu',
-      'price': 100-300,
+      'price': '100-300',
       'count': 3,
       'image': '/img/ortopeda.jpg',
-      'contact': 604-353-234,
-      'symptoms': ['złamanie, uraz']
+      'symptoms': ['złamanie, uraz'],
+      'example': [
+        {
+            'name': 'lek. Kazimierz Lis',
+            'contact': '604-353-234'
+        },
+        {
+            'name': 'lek. Łukasz Stępień',
+            'contact': '324-554-235'
+        },
+        {
+            'name': 'lek. Łukasz Stępień',
+            'contact': '454-134-236'
+        },],
 
     },
     {
       '_id': '2',
       'name': 'Kardiolog',
       'description': 'Diagnostyka i leczenie chorób układu krwionośnego',
-      'price': 100-350,
+      'price': '100-350',
       'count': 3,
       'image': '/img/kardiolog.jpg',
-      'contact': 304-353-234,
-      'symptoms': ['ból w klatce piersiowej, omdlenia']
+      'symptoms': ['ból w klatce piersiowej, omdlenia'],
+      'example': [
+        {
+            'name': 'lek. Wiktoria Bąk',
+            'contact': '564-353-234'
+        },
+        {
+            'name': 'lek. Mikołaj Pietrzak',
+            'contact': '675-767-235'
+        },
+        {
+            'name': 'lek. Jacek Baranowski',
+            'contact': '456-353-236'
+        },],
     },
     {
       '_id': '3',
       'name': 'Neurolog',
       'description': 'Badanie czynności układu nerwowego oraz reakcji organizmu na bodźce',
-      'price': 200-300,
+      'price': '200-300',
       'count': 3,
       'image': '/img/neurolog.jpg',
-      'contact': 632-353-234,
-      'symptoms': ['nerwica, Alzheimer, ból w klatce piersiowej']
+      'symptoms': ['nerwica, Alzheimer, ból w klatce piersiowej'],
+      'example': [
+        {
+            'name': 'lek. Izabela Gajewska',
+            'contact': '232-353-234'
+        },
+        {
+            'name': 'lek. Zygmunt Marciniak',
+            'contact': '778-353-235'
+        },
+        {
+            'name': 'lek. Artur Borkowski',
+            'contact': '876-353-236'
+        },],
     },
     {
       '_id': '4',
       'name': 'Onkolog',
       'description': 'Profilaktyka, diagnoza oraz leczenie schorzeń nowotworowych',
-      'price': 140-500,
+      'price': '140-500',
       'count': 3,
       'image': '/img/onkolog.jpg',
-      'contact': 423-134-234,
       'symptoms': ['guz', 'zmiany na skórze']
     },
     {
       '_id': '5',
       'name': 'Chirurg',
       'description': 'Diagnostyka i leczenie schorzeń tkanek miękkich tj. tkanki podskórnej i skóry, narządów wewnętrznych układu trawiennego, brzucha, pęcherza moczowego, klatki piersiowej, szyi oraz mięśni',
-      'price': 150-600,
+      'price': '150-600',
       'count': 3,
       'image': '/img/chirurg.jpg',
       'contact': 534-353-455,
-      'symptoms': ['guz', 'zmiany na skórze', 'ból brzucha']
+      'symptoms': ['guz', 'zmiany na skórze', 'ból brzucha'],
+      'example': [
+        {
+            'name': 'lek. Ireneusz Sokolski',
+            'contact': '604-353-222'
+        },
+        {
+            'name': 'lek. Alicja Szymańska',
+            'contact': '878-353-544'
+        },
+        {
+            'name': 'lek. Maciej Adamski',
+            'contact': '234-353-478'
+        },],
     },
     {
       '_id': '6',
       'name': 'Psychiatra',
       'description': 'Diagnostyka oraz leczenie chorób i zaburzeń psychicznych',
-      'price': 80-200,
+      'price': '80-200',
       'count': 3,
       'image': '/img/psychiatra.jpg',
-      'contact': 534-353-234,
-      'symptoms': ['nerwica', 'stany depresyjne', 'ból brzucha']
+      'symptoms': ['nerwica', 'stany depresyjne', 'ból brzucha'],
+      'example': [
+        {
+            'name': 'lek. Michał Górski',
+            'contact': '344-322-234'
+        },
+        {
+            'name': 'lek. Wiktoria Zalewska',
+            'contact': '742-135-235'
+        },
+        {
+            'name': 'lek. Aleksandra Jaworska',
+            'contact': '875-353-236'
+        },],
     },
     {
       '_id': '7',
       'name': 'Lekarz Ogólny',
       'description': 'Diagnostyka i leczenie chorób, przeprowadzanie bilansów zdrowia i kwalifikacji do szczepień',
-      'price': 60-200,
+      'price': '60-200',
       'count': 3,
       'image': '/img/ogolny.jpg',
-      'contact': 564-344-234,
-      'symptoms': ['ból brzucha', 'gorączka', 'ból w klatce piersiowej', 'omdlenia', 'kaszel']
+      'symptoms': ['ból brzucha', 'gorączka', 'ból w klatce piersiowej', 'omdlenia', 'kaszel'],
+      'example': [
+        {
+            'name': 'lek. Jan Kowalski',
+            'contact': '604-353-234'
+        },
+        {
+            'name': 'lek. Marta Wróblewska',
+            'contact': '432-353-235'
+        },
+        {
+            'name': 'lek. Bartosz Raczewski',
+            'contact': '765-353-236'
+        },],
     },
     {
       '_id': '8',
       'name': 'Alergolog',
       'description': 'Diagnostyka i leczenie chorób alergicznych',
-      'price': 90-350,
+      'price': '90-350',
       'count': 3,
       'image': '/img/alergolog.jpg',
-      'contact': 566-353-234,
-      'symptoms': ['zmiany na skórze', 'wysypka']
+      'symptoms': ['zmiany na skórze', 'wysypka'],
+      'example': [
+        {
+            'name': 'lek. Jan Alergia',
+            'contact': '534-353-565'
+        },
+        {
+            'name': 'Anna Nowak',
+            'contact': '865-353-343'
+        },
+        {
+            'name': 'Marek Zdzisłowski',
+            'contact': '554-353-664'
+        },],
     },
   ]
   
--- frontend/src/screens/DoctorScreen.js
+++ frontend/src/screens/DoctorScreen.js
@@ -3,7 +3,7 @@ import {useParams} from 'react-router-dom'
 //import Doctor from '../components/Doctor'
 import doctors from '../doctors'
 import {Link} from 'react-router-dom'
-import { Row,Col,Image, ListGroup } from 'react-bootstrap'
+import { Row,Col,Image, ListGroup, ListGroupItem } from 'react-bootstrap'
 
 
 function DoctorScreen({match}) {
@@ -16,27 +16,42 @@ function DoctorScreen({match}) {
                 <Col md={6}>
                     <Image src={doctor.image} alt={doctor.name} fluid />
                 </Col>
-                <Col md={2}>
+                <Col md={4}>
                     <ListGroup variant="flush">
                         <ListGroup.Item>
-                            <h3>{doctor.name}</h3>
+                            <h3 style={{ marginLeft: '80px' }}>{doctor.name}</h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            Cena: 100{doctor.price}zł
+                            Cena: {doctor.price}zł
                         </ListGroup.Item>
                         <ListGroup.Item>
                             Opis: {doctor.description}
                         </ListGroup.Item>
                     </ListGroup>
                 </Col>
-                <Col md={2}>
+                {/* <Col md={2}>
                     <ListGroup variant="flush">
                         <ListGroup.Item>
-                          
+                            <h3>Przykładowi lekarze</h3>
                         </ListGroup.Item>
-                    </ListGroup>
-
-                
+                        </ListGroup>
+                           <ListGroup>
+                            
+                           </ListGroup>
+                           </Col> */}
+            </Row>
+            <Row>
+                <Col md={6}>
+                <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3 style={{ marginLeft: '100px' }}> Zalecani lekarze</h3>
+                            {doctor.example.map(exampleDoctor => (
+                    <ListGroup.Item>
+                        {exampleDoctor.name} - kontakt: {exampleDoctor.contact}
+                    </ListGroup.Item>
+                ))}
+                </ListGroup.Item>
+                </ListGroup>
                 </Col>
             </Row>
         </div>
